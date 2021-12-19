9a43d98f9a7a91ce01625eeebcc2ab22e1d38a84
ShinMini <77220824+ShinMini@users.noreply.github.com>
2021-12-19 14:37:56 +0900

    [git clone] files example file about using practice

---
+++ virtualevent-demo/src/main/java/io/getstream/chat/virtualevent/feature/event/list/EventsFragment.kt
@@ -0,0 +1,51 @@
+package io.getstream.chat.virtualevent.feature.event.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.getstream.chat.virtualevent.AppConfig
+import io.getstream.chat.virtualevent.R
+import io.getstream.chat.virtualevent.databinding.FragmentEventsBinding
+import io.getstream.chat.virtualevent.feature.event.detail.EventDetailsActivity
+
+/**
+ * Fragment with a list of conference events.
+ */
+class EventsFragment : Fragment() {
+
+    private var _binding: FragmentEventsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentEventsBinding.inflate(inflater, container, false)
+            .apply { _binding = this }
+            .root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.event1Include.event1CardView.setOnClickListener {
+            startActivity(
+                EventDetailsActivity.createIntent(
+                    requireContext(),
+                    AppConfig.LIVESTREAM_ESG_DATA,
+                    requireContext().getString(R.string.overview_hypercube_title)
+                )
+            )
+        }
+        binding.event2Include.event2CardView.setOnClickListener {
+            startActivity(
+                EventDetailsActivity.createIntent(
+                    requireContext(),
+                    AppConfig.LIVESTREAM_DATA_STRATEGY,
+                    requireContext().getString(R.string.overview_layers_title)
+                )
+            )
+        }
+    }
+}
